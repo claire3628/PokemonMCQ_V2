5566764188d98f2c07b80a0a0b7e66c10c3f08bb
claire chang <claire3628@gmail.com>
2025-04-06 22:43:59 +0800

    Initial Commit

---
+++ PokemonMCQ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonMCQ
+//
+//  Created by Claire Chang on 2025/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
